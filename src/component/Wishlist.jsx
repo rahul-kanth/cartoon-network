e82beed79cb828059370dbe3b7d9a064e88c1011
rahul kanth <you@example.com>
2022-09-10 11:39:35 +0530

    first commit

--- src/component/Wishlist.jsx
+++ src/component/Wishlist.jsx
@@ -1,27 +1,31 @@
-import { useState } from "react";
+import { useEffect,useState } from "react";
 import useFetch from "./useFetch";
+import Animelist from "./Animelist";
 
 const Wishlist = () => {
     let{data:animes,pending,error}=useFetch("https://api.jikan.moe/v4/anime");
-
+    
+    //const[wishlistAnime, setwishlistAnime] = useState(null);
 
     const[wish,setwish] = useState(null);
     
-    useFetch(()=>{
+    useEffect(()=>{
         
-        var wish = localStorage.getItem("wish");
+        let wish = localStorage.getItem("wish");
         wish = JSON.parse(wish);
         setwish(wish);
 
     },[])
     return ( 
         <div>
+
+            {error && <h1>data not found</h1>}
   
         {pending && <div class="spinner-border text-danger" role="status">
                     <span class="visually-hidden"> Loading....</span>
          </div>}
     
-    {animes && <Animeslist animes={animes.filter((animes)=>{return wish.includes(animes.mal_id)})}  /> }
+    {animes && <Animelist animes={animes.filter((animes)=>{return wish.includes(animes.mal_id)})}  /> }
     
        </div>     
      );
